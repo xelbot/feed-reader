c1bd8fcf9671ed60c0281d4bb0569401ca782955
igor-chepurnoi <igorzfort@gmail.com>
2016-12-29 18:33:43 +0200

    added users fixture

---
+++ src/Xelbot/TestBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Xelbot\TestBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUserData extends AbstractFixture implements ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var int
+     */
+    protected $countOfGeneratedUsers = 100;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('xelbot.user.user_manager');
+        $userManager->createUser('admin@example.org', 'admin', 'test', true);
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i <= $this->countOfGeneratedUsers; $i++) {
+            $userManager->createUser($faker->email, $faker->userName, 'test', true);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
